f6d027fa38be0a30afa0325a434cfa3cfa5ccae8
Alexander Orlov <eadenink@gmail.com>
2025-01-10 00:40:54 +0400

    feat: Draw player

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import pygame
 
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from player import Player
 
 
 def main():
     pygame.init()
+
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    screen_center = (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    player = Player(*screen_center)
+
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -14,7 +22,10 @@ def main():
 
         screen.fill((0, 0, 0))
 
+        player.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60)
 
 
 if __name__ == "__main__":
